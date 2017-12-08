6144cd8d08a67239450f1248ea609c6f96a63ca1
Simon Herteby <s.herteby@gmail.com>
2017-12-08 16:20:37 +0100

    some fixes

--- client/main.js
+++ client/main.js
@@ -4,16 +4,12 @@ import 'vue-clicky'
 
 import {GraphicalGrapher} from 'meteor/herteby:graphical-grapher'
 
-Vue.prototype.$autoWatch = function(fn, options){
-	this.$watch(fn, () => null, {deep:true,immediate:true})
-}
-
 new Vue({
 	el:'#app',
 	...GraphicalGrapher
 })
 
-/*
+/* Uncomment this and comment out the code above to test the React wrapper
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {GraphicalGrapherReact} from 'meteor/herteby:graphical-grapher'
